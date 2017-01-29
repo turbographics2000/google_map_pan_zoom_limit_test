80087904a9bac3115a0d05b2cd81f30c6145b742
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-30 06:52:57 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,18 +1,9 @@
-import limitMap from 'google-map-bounds-limit';
-
-const minZoomLevel = 3;
 const map = new google.maps.Map(document.getElementById('mapArea'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
 });
-map.addListener('zoom_changed', zoomChanged);
 var maxBounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(-85.00, -180.00), 
     new google.maps.LatLng(85.00, 180.00)
 );
-
 limitMap(map, maxBounds);
-
-function zoomChanged() {
-    if (map.getZoom() < minZoomLevel) map.setZoom(minZoomLevel);
-}
