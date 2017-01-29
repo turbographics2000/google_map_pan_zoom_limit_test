97b9a486815a7d89a4a576ab3a1bd208e62d4b72
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-30 06:51:37 +0900

    Update google-map-bounds-limit.js

--- docs/google-map-bounds-limit.js
+++ docs/google-map-bounds-limit.js
@@ -1,10 +1,3 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.limitMap = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = limitMap;
 var TILE_SIZE = 256;
 var lastValidCenter = null;
 var ignoreNextMapMove = false;
@@ -146,9 +139,3 @@ function limitMap(map, maxBounds) {
   // call this once, because the next zoom level might already be too low
   limitMapMinZoom(map);
 }
-
-// for standalone build
-module.exports = limitMap;
-
-},{}]},{},[1])(1)
-});
